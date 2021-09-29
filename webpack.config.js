8e438283221a2eccd1239a1ffe02743f237192b0
Nicolas Dao <nic@neap.co>
2021-09-29 14:40:58 +1000

    fix: All vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -6,43 +6,47 @@
  * LICENSE file in the root directory of this source tree.
 */
 const path = require('path')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require("terser-webpack-plugin")
 
 const env = process.env.WEBPACK_ENV
 const outputfilename = "graphqls2s"
-const buildPlugins = [
-    new UglifyJSPlugin()
-]
-const { plugins, outputfile } = env == "build" 
-    ? { plugins: buildPlugins, outputfile: `${outputfilename}.min.js` } 
-    : { plugins: [], outputfile: `${outputfilename}.js` } 
+const prod = env == "build"
+
+const { plugins, outputfile } = prod
+	? { plugins: [], outputfile: `${outputfilename}.min.js` } 
+	: { plugins: [], outputfile: `${outputfilename}.js` } 
 
 module.exports = {
-    entry: [
-        'babel-polyfill',
-        './src/graphqls2s.js'
-    ],
-    output: {
-        path: __dirname + '/lib',
-        filename: outputfile,
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    module: {
-        loaders: [{
-            loader: "babel-loader",
-            exclude: [
-                path.resolve(__dirname, "node_modules")
-            ],
-            // Only run `.js` and `.jsx` files through Babel
-            test: /\.jsx?$/,
-            // Options to configure babel with
-            query: {
-                plugins: ['transform-runtime'],
-                presets: ['es2015'],
-            }
-        }, ]
-    },
-    devtool: 'source-map',
-    plugins: plugins
+	entry: [
+		'babel-polyfill',
+		'./src/graphqls2s.js'
+	],
+	output: {
+		path: __dirname + '/lib',
+		filename: outputfile,
+		libraryTarget: 'umd',
+		umdNamedDefine: true,
+		globalObject: 'this'
+	},
+	module: {
+		rules: [{
+			loader: "babel-loader",
+			exclude: [
+				path.resolve(__dirname, "node_modules")
+			],
+			// Only run `.js` and `.jsx` files through Babel
+			test: /\.jsx?$/,
+			// Options to configure babel with
+			options: {
+				plugins: ['transform-runtime'],
+				presets: ['es2015'],
+			}
+		}, ]
+	},
+	devtool: 'source-map',
+	plugins: plugins,
+	optimization: {
+		minimize: prod,
+		minimizer: [new TerserPlugin()]
+	}
 }
